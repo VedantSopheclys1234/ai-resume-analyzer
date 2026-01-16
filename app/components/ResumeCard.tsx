326405c80240c39db23e5a32ddd6a0ab1c415c1d
Vedant Sampate <vedantsampate98@gmail.com>
2026-01-16 15:06:39 +0530

    Authentication

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router'
+import ScoreCircle from "./ScoreCircle"
+import { resume } from 'react-dom/server';
+
+interface Resume {
+  id: string;
+  companyName: string;
+  jobTitle: string;
+  feedback: {
+    overallScore: number;
+  };
+  imagePath: string;
+}
+
+const ResumeCard = ( {resume: { id, companyName, jobTitle, feedback, imagePath }}: { resume: Resume }) => {
+  return (
+    <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
+        <div className='flex flex-col gap-2'>
+            <div className='flex flex-col gap-2'>
+                <h2 className='!text-black font-bold break-words'>{companyName}</h2>
+                <h3 className='text-lg break-words text-gray-500'>{jobTitle}</h3>
+            </div>
+            <div className='flex-shrink-0'>
+                <ScoreCircle score={feedback.overallScore} />
+            </div>
+        </div>
+        <div className='gradient-border animate-in fade-in duration-1000'>
+            <div className='w-full h-full'>
+                <img src={imagePath} 
+                alt="resume" 
+                className='w-full h[350px] max-sm:h-[200px] object-cover object-top'
+                 />
+            </div>
+        </div>
+    </Link>
+  )
+}
+
+export default ResumeCard
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,60 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import Navbar from "~/components/Navbar";
+import Auth from "./auth";
+
+import { usePuterStore } from "~/lib/puter";
+import { Link, useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { resumes } from "~/constants";
+
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return[
+    { title: "Resumeaizer" },
+    { name: "description", content: "Smart Resume analyzer to find your dream job" },
+  ];  
 }
 
 export default function Home() {
-  return <Welcome />;
+  const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track you Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    
+
+      {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
+
+      {!loadingResumes && resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+
+      {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+      )}
+    </section>
+  </main>
 }
